a0e6c184922ae3cf5bac626fc9e3ad7e1ce74b1d
DanWilliams24 <01danielwilliams@gmail.com>
2018-03-03 01:33:53 -0500

    Finalized Course api GET feature(CURRENTLY DISABLED FOR TESTS)

--- routes/grades.js
+++ routes/grades.js
@@ -3,71 +3,47 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var Nightmare = require('nightmare'),
-  nightmare = Nightmare({
-    show: true
-  });
 var async = require('async');
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({show: true})
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+
 
 router.get('/', function (req, res, next) {
-    /* if(!req.query.factorial === null){
-        var number = req.query.num;
-        var answer = factorial(number);
-        res.send('Answer: ' + answer);
-    }else if((!req.query.a ===undefined) && (!req.query.x === undefined)){ 
-        
-    }
-    var factorial = function (num) {
-        if (num === 0 ) {
-            return 1;
-        } else {
-            return num*factorial(num - 1);
-        }
-    }
-    var logarithm = function(val){
-        return Math.log(val);
-    }
-    var changeOfBase = function(a, x){
-        return Math.log(x)/Math.log(a);
-    }
-    if(req.query.x > 0){
-        res.send('Answer: ' + changeOfBase(req.query.a,req.query.x));
-    }else{
-        res.send('Answer: ' + logarithm(req.query.val));
-    }
-    */
+  var access_token = '?access_token=2873~OAXT1qIVPKRgCBmn6YI32BlhZhvaF7lZZTm1SvasfeCR2KbsFhatDYSLloJDxKY4';
+  var base_uri = 'https://mcpsmd.instructure.com'
+  var courses_url = '/api/v1/courses';
+  var announcements_url = '/';
+  var result = base_uri + courses_url + access_token;
+  console.log(result);
+  var json = {
+    "page": "Student and Parent Sign In",
+    "copyright": "Copyright © 2005-2016 PowerSchool Group LLC and/or its affiliate(s). ",
+    "htmlData": "hello"
+}
+  /* request(result).pipe(fs.createWriteStream('courseData.json')
+  .on('close', function(){
+    var urlFrags = __dirname.split('/');
+    console.log(__dirname.split('/'));
+    var formedURL = '';
+    for(let i = 1; i < urlFrags.length-1; i++ ){
+      const element = urlFrags[i];
+      formedURL += '/' + element;
+    } */
+    var urlFrags = __dirname.split('/');
+    console.log(__dirname.split('/'));
+    var formedURL = '';
+    for(let i = 1; i < urlFrags.length-1; i++ ){
+      const element = urlFrags[i];
+      formedURL += '/' + element;
+    } 
+    res.sendFile('courseData.json',{root: formedURL});
+    //res.send(json);
+  //}));
 
-   nightmare
-   //load a url
-   .goto('https://portal.mcpsmd.org/public/')
-   //simulate typing into an element identified by a CSS selector
-   //here, Nightmare is typing into the search bar
-   .type('input[name="account"]', '341086')
-   .type('input[name="pw"]', 'Ryeh5070')
-   //click an element identified by a CSS selector
-   //in this case, click the search button
-   .click('#btn-enter')
-   //wait for an element identified by a CSS selector
-   //in this case, the body of the results
-   .wait('#main')
-   //execute javascript on the page
-   //here, the function is getting the HREF of the first search result
-   .evaluate(function() {
-     res.send(document.body.innerHTML);
-     return document.body.innerHTML;
-   })
-   //end the Nightmare instance along with the Electron instance it wraps
-   .end()
-   //run the queue of commands specified, followed by logging the HREF
-   .then(function(result) {
-     res.send(result);
-   })
-   //catch errors if they happen
-   .catch(function(error){
-     console.error('an error has occurred: ' + error);
-   });
-    
-    //res.send("Output JSON here");
+  
 });
 
 module.exports = router;
